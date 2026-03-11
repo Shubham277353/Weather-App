92e44c9d9333b292bbafd3c026fde50374432855
Shubham277353 <golubhandari405@gmail.com>
2026-03-11 23:46:50 +0530

    feat: add form to search the city

--- src/apiLogic.js
+++ src/apiLogic.js
@@ -1,5 +1,5 @@
-export async function apiLogic(city) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=LARVWFZYTTCPBN96DFXXN2MLY`);
-    const data = await response.json();
-    console.log(data);
-}
+// export async function apiLogic(city) {
+//     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=LARVWFZYTTCPBN96DFXXN2MLY`);
+//     const data = await response.json();
+//     console.log(data.days[0].feelslike);
+// }
