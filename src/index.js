68399081d920ad3fa89878f20e459ca5b90ffdb4
Shubham277353 <golubhandari405@gmail.com>
2026-03-12 00:23:47 +0530

    feat: add searching DOM logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./style.css";
 import { apiLogic } from "./apiLogic";
-apiLogic("Delhi");
+import searchDOM from "./searchDOM";
+
+searchDOM();
--- src/searchDOM.js
+++ src/searchDOM.js
@@ -1,8 +1,15 @@
-const searchBtn = document.getElementById("search-btn");
-const searchInput = document.getElementById("search-box");
+import { apiLogic } from "./apiLogic";
 
-export default searchBtnDom(){
-    searchBtn.addEventListener("click",()=>{
+const searchBtn = document.getElementById("search-box");
+const searchInput = document.getElementById("search-input");
+const form = document.querySelector("form");
 
+export default function searchBtnDom() {
+
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const city = searchInput.value;
+        if(!city) return;
+        apiLogic(city);
     })
 }
